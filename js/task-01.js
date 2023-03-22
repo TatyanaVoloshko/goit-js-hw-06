8056d557c46682a1b4b546e858917ba2c4d1f0c1
TatyanaVoloshko <106927709+TatyanaVoloshko@users.noreply.github.com>
2023-03-22 12:42:26 +0100

    corrected some mistakes

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,6 @@ const categoryItems = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoryItems.length}`);
 
 categoryItems.forEach(item => {
-    const category = item.querySelector('h2').textContent;
-    const elements = item.querySelectorAll('ul > li').length
-    console.log(`Category: ${category}\nElemehts: ${elements}`)
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`)
 })
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const changeInput = document.querySelector('input#font-size-control');
 const changeFontSize = document.querySelector('span#text');
 
-changeInput.addEventListener('change', event => {
+changeInput.addEventListener('input', event => {
     changeFontSize.style.fontSize = `${event.target.value}px`
 });
